2b7d79a5ce2c6f60abec65781b05470da4755199
cdawgtheboss <c_dawgkong@yahoo.com>
2020-05-27 12:56:10 -0400

    handle-data

--- src/App.js
+++ src/App.js
@@ -11,13 +11,15 @@ function App(props) {
     const apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=Danielsville&appid=43539669c18751f12183dc36c1143086'
     fetch(apiUrl)
     .then(res => res.json())
-    .then(res => res.list.forEach(a => setWeatherData(a.weather[0].main)))
+    .then(res => res.list.map(a => setWeatherData(weatherData)))
   }, [])
-  return (
-    <div className="App">
-      <WeatherItem name={""}/>
-    </div>
-  );
+      weatherData.map(weather => {
+      return (
+        <div className="App">
+        <WeatherItem temp={weatherData.temp}/>
+      </div>
+    );
+  })
 }
 
 
--- src/components/weather-item.jsx
+++ src/components/weather-item.jsx
@@ -6,17 +6,11 @@ const kelvinToFahrenheit = (k) => {
 }
 
 const WeatherItem = (props) => {
-  const [weatherData, setWeatherData] = useState()
-  useEffect( () =>{
-    const apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=Danielsville&appid=43539669c18751f12183dc36c1143086'
-    fetch(apiUrl)
-    .then(res => res.json())
-    .then(res => res.list.forEach(a => console.log(a.weather[0].main)))
-  }, [])
+  
   
   return(
     <div>
-      <h3>Date</h3>
+      <h3>{props.date}</h3>
       weather image
       temperature
     </div>
