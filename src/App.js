b19cb68c5980122913c114c69019d0ca2c47ff57
cdawgtheboss <c_dawgkong@yahoo.com>
2020-05-30 02:55:26 -0400

    data-handled

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React, {useEffect, useState} from 'react';
-import './App.css';
-import WeatherItem from './components/weather-item';
-// import WeatherRow from './components/weather-row';
+import React, { useEffect, useState } from 'react';
 
-
-function App(props) {
-
-  const [weatherData, setWeatherData] = useState()
-  useEffect( () =>{
-    const apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=Danielsville&appid=43539669c18751f12183dc36c1143086'
+function App() {
+  const apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=Danielsville&appid=43539669c18751f12183dc36c1143086'
+  const [weatherData, setWeatherData] = useState("this is my initial state")
+  useEffect( () => {
     fetch(apiUrl)
     .then(res => res.json())
-    .then(res => res.list.map(a => setWeatherData(weatherData)))
+    .then(data => setWeatherData(data))
+    
+
+    
   }, [])
-      weatherData.map(weather => {
-      return (
-        <div className="App">
-        <WeatherItem temp={weatherData.temp}/>
-      </div>
-    );
-  })
+  console.log("weatherData", weatherData)
+  return(
+    <div>
+      this is my app
+    </div>
+  )
 }
 
-
 export default App;
+
